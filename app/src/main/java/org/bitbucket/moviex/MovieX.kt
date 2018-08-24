7a05b5fb8de26dc5416029a981352c3079d49102
Emmanuel Victor Garcia <emmanuel.garcia@xurpas.com>
2018-08-24 18:47:47 +0800

    Initial commit

---
+++ app/src/main/java/org/bitbucket/moviex/MovieX.kt
@@ -0,0 +1,13 @@
+package org.bitbucket.moviex
+
+import android.app.Application
+
+class MovieX: Application() {
+
+    private val TMDB_API_KEY = "12bc7d66c68d1cfad629138b2f2b46e2"
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
